7a21331237343322d47f79e41ed2a62552851a3f
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-24 18:24:09 +0300

    Adding a NoteColor to a Note

--- app/src/main/java/com/topic2/android/notes/util/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/util/components/Note.kt
@@ -10,16 +10,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
 
-
 @Composable
 fun Note(){
     Row(modifier = Modifier.fillMaxWidth()) {
 
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
-        )
+        NoteColor(
+            color = rwGreen,
+            size =40.dp,
+            padding =4.dp,
+            border=1.dp)
         Column(modifier = Modifier.weight(1f)) {
             Text(text = "Заголовок", maxLines = 1)
             Text(text = "Содержание", maxLines = 1)
@@ -32,11 +31,7 @@ fun Note(){
 
     }
 
-}
-
-
-
-@Preview
+}@Preview
 @Composable
 fun NotePreview(){
     Note()
