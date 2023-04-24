f2c18410d5b4b0d0e7c571a0e242ffbc7a416ec0
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-24 18:56:22 +0300

    Centering text and checkbox

--- app/src/main/java/com/topic2/android/notes/util/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/util/components/Note.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -33,14 +34,19 @@ fun Note() {
             padding = 4.dp,
             border = 1.dp
         )
-        Column(modifier = Modifier.weight(1f)) {
+        Column(modifier = Modifier
+            .weight(1f)
+            .align(Alignment.CenterVertically)
+        ) {
             Text(text = "Заголовок", maxLines = 1)
             Text(text = "Содержание", maxLines = 1)
         }
 
         Checkbox(
             checked = false, onCheckedChange = {},
-            modifier = Modifier.padding(start = 8.dp)
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
         )
 
     }
