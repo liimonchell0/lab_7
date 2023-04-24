45f8ae5ac506dc16b739c535a260836d582eda55
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-24 16:28:34 +0300

    Creating a Note Component

---
+++ app/src/main/java/com/topic2/android/notes/util/components/Note.kt
@@ -0,0 +1,16 @@
+package com.topic2.android.notes.util.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Note(){
+}
+
+
+
+@Preview
+@Composable
+fun NotePreview(){
+    Note()
+}
