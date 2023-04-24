d44ca6d1b934f32e7d1d988d0d052dbe9c80e83e
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-24 18:12:10 +0300

    Usability improvements

--- app/src/main/java/com/topic2/android/notes/util/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/util/components/NoteColor.kt
@@ -13,16 +13,24 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor(){
+fun NoteColor(
+    color: Color,
+    size: Dp,
+    padding: Dp = 0.dp,
+    border: Dp
+){
+
+
     Box(
         modifier = Modifier
-            .padding(4.dp)
-            .size(40.dp)
+            .padding(padding)
+            .size(size)
             .clip(CircleShape)
-            .background(Color.Red)
+            .background(color)
             .border(
                 BorderStroke(
                     2.dp,
@@ -37,6 +45,6 @@ fun NoteColor(){
 @Preview
 @Composable
 fun NoteColorPreview(){
-    NoteColor()
+    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
 }
 
