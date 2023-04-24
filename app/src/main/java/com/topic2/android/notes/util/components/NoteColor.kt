a8f64ad47f7c851f9f69d5b9e8010a10cd1f5fc5
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-24 18:07:04 +0300

    Adding padding

--- app/src/main/java/com/topic2/android/notes/util/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/util/components/NoteColor.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,6 +19,7 @@ import androidx.compose.ui.unit.dp
 fun NoteColor(){
     Box(
         modifier = Modifier
+            .padding(4.dp)
             .size(40.dp)
             .clip(CircleShape)
             .background(Color.Red)
