c4fc3b142403902acb32b2c2048666ac536eb437
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2023-04-24 19:04:15 +0300

    Applying padding

--- app/src/main/java/com/topic2/android/notes/util/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/util/components/Note.kt
@@ -29,10 +29,12 @@ fun Note() {
     ) {
 
         NoteColor(
-            modifier = Modifier.align(Alignment.CenterVertically),
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(start = 16.dp, end = 16.dp),
+
             color = rwGreen,
             size = 40.dp,
-            padding = 4.dp,
             border = 1.dp
         )
         Column(modifier = Modifier
--- app/src/main/java/com/topic2/android/notes/util/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/util/components/NoteColor.kt
@@ -22,20 +22,18 @@ fun NoteColor(
     modifier: Modifier = Modifier,
     color: Color,
     size: Dp,
-    padding: Dp = 0.dp,
     border: Dp
 ){
 
 
     Box(
         modifier = modifier
-            .padding(padding)
             .size(size)
             .clip(CircleShape)
             .background(color)
             .border(
                 BorderStroke(
-                    2.dp,
+                    border,
                     SolidColor(Color.Black)
 
                 ),
@@ -47,6 +45,6 @@ fun NoteColor(
 @Preview
 @Composable
 fun NoteColorPreview(){
-    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
+    NoteColor(color = Color.Red, size = 40.dp, border = 2.dp)
 }
 
